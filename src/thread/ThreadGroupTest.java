630cf50d0709effddfcd497a72f2d99fc7ca1b88
siegezhang <z1178858918@qq.com>
2017-09-01 00:50:37 +0800

    thread的用法

---
+++ src/thread/ThreadGroupTest.java
@@ -0,0 +1,51 @@
+package thread;
+
+/**
+ * Created by siege on 2017/8/31.
+ */
+public class ThreadGroupTest {
+    public static void main(String[] args) {
+        ThreadGroup threadGroupMain=Thread.currentThread().getThreadGroup();
+        System.out.println("current ThreadGroup name ="+threadGroupMain.getName());
+
+        MyRunnable myRunnable=new MyRunnable();
+
+        ThreadGroup childThreadGroup1=new ThreadGroup("childThreadGroup1");
+        Thread thread1=new Thread(childThreadGroup1,myRunnable,"Thread-1");
+        thread1.start();
+
+        ThreadGroup childThreadGroup2=new ThreadGroup("childThreadGroup2");
+        Thread thread2=new Thread(childThreadGroup2,myRunnable,"Thread-2");
+        thread2.start();
+
+        System.out.println("Currently active groups  in main ThreadGroup ="+threadGroupMain.activeGroupCount());
+        System.out.println("Currently active threads in main ThreadGroup ="+threadGroupMain.activeCount());
+
+        System.out.println("\nchildThreadGroup1's parent ThreadGroup ="+childThreadGroup1.getParent().getName());
+        System.out.println("Thread-1's ThreadGroup ="+thread1.getThreadGroup().getName());
+
+        System.out.println("\nchildThreadGroup2's parent ThreadGroup ="+childThreadGroup2.getParent().getName());
+        System.out.println("Thread-2's ThreadGroup ="+thread2.getThreadGroup().getName());
+
+        System.out.println("\n--- list() method - Prints information about thread group ---");
+        threadGroupMain.list();
+
+
+
+
+
+
+
+    }
+}
+class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/thread/producerandconsumer/withoutwaitnotify/ProducerConsumerWithoutWaitNotify.java
@@ -0,0 +1,72 @@
+package thread.producerandconsumer.withoutwaitnotify;
+
+import java.util.ArrayList;
+
+/**
+ * Created by siege on 2017/9/1.
+ */
+
+class Producer implements Runnable{
+    boolean productionInProcess;
+    ArrayList<Integer> list;
+
+    public Producer() {
+        productionInProcess=true;
+        list=new ArrayList<Integer>();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <=10 ; i++) {
+            list.add(i);
+            System.out.println("Producer is still Producing, Produced : "+i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        productionInProcess=false;
+    }
+}
+
+class Consumer extends Thread{
+    Producer producer;
+
+    public Consumer(Producer producer) {
+        this.producer = producer;
+    }
+
+    @Override
+    public void run() {
+        while (producer.productionInProcess){
+            System.out.println("Consumer waiting for production to get over.");
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Production is over, consumer can consume.");
+        int producerSize=producer.list.size();
+        for (int i = 0; i <producerSize ; i++) {
+            System.out.println("Consumed : "+producer.list.remove(0)+" ");
+        }
+    }
+}
+public class ProducerConsumerWithoutWaitNotify {
+    public static void main(String[] args) {
+        Producer producer=new Producer();
+        Consumer consumer=new Consumer(producer);
+
+        Thread producerThread=new Thread(producer,"producerThread");
+        Thread consumerThread=new Thread(consumer,"consumerThread");
+
+        producerThread.start();
+        consumerThread.start();
+    }
+
+
+
+
+}
