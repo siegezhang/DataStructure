08540580ddf615bceb99c8fd0aaf9b6ca944288d
siegezhang <z1178858918@qq.com>
2018-02-28 00:29:08 +0800

    代码重构

--- src/main/java/basic/bitmap/BitSetTest.java
+++ src/main/java/basic/bitmap/BitSetTest.java
@@ -1,32 +1,62 @@
 package basic.bitmap;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.BitSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 
 /**
  * Created by siege on 2017/9/2.
  */
 public class BitSetTest {
-    public static void main(String[] args) {
-        BitSet bitSet1=new BitSet();
-        System.out.println(bitSet1.isEmpty()+"---"+bitSet1.size());
+    private static BitSet bitSet1;
+    private static BitSet bitSet2;
+    private static int[] data={2,42,5,6,18,33,15,25,31,28,37};
+
+    @BeforeAll
+    @DisplayName("相关成员变量初始化")
+    static void init(){
+        bitSet1=new BitSet();
+        bitSet2=new BitSet(getMaxValue(data));
+    }
+    @Test
+    @DisplayName("BitSet基本特性测试")
+    void basicPropertiesTest() {
+
+        assertTrue(bitSet1.isEmpty());
+        assertEquals(64,bitSet1.size());
+
+
         bitSet1.set(0);
-        System.out.println(bitSet1.isEmpty()+"---"+bitSet1.size());
+        assertFalse(bitSet1.isEmpty());
+        bitSet1.clear();
+        assertTrue(bitSet1.isEmpty());
+        assertEquals(64,bitSet1.size());
+
         bitSet1.set(1);
-        System.out.println(bitSet1.isEmpty()+"---"+bitSet1.size());
-        System.out.println(bitSet1.get(65));
-        System.out.println(bitSet1.isEmpty()+"--"+bitSet1.size());
+        assertEquals(64,bitSet1.size());
+
+        assertFalse(bitSet1.get(65));
+        assertEquals(64,bitSet1.size());
+
         bitSet1.set(65);
-        System.out.println(bitSet1.isEmpty()+"--"+bitSet1.size());
-        System.out.println("--------------------------");
+        assertEquals(128,bitSet1.size());
 
+    }
 
-        //bitmap来实现对不重复的数据进行排序
-        int[] data={2,42,5,6,18,33,15,25,31,28,37};
-
-        BitSet bitSet2=new BitSet(getMaxValue(data));
-        System.out.println("size="+bitSet2.size());
+    @Test
+    @DisplayName("BitSet来实现对不重复的数据进行排序")
+    void sortTest(){
+        assertEquals(64,bitSet2.size());
         putValueIntoBitSet(data,bitSet2);
         sort(bitSet2,data);
         System.out.println(Arrays.toString(data));
@@ -49,7 +79,7 @@ public class BitSetTest {
 
     private static void putValueIntoBitSet(int[] data, BitSet bitSet2) {
         for (int i = 0; i <data.length ; i++) {
-            bitSet2.set(data[i],true);
+            bitSet2.set(data[i]);
         }
     }
 
---
+++ src/main/java/regex/RegexTest.java
@@ -0,0 +1,51 @@
+package regex;
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RegexTest {
+
+    @BeforeAll
+    @DisplayName("表明在所有测试方法运行之前执行的方法")
+    static void beforeAll(){
+        System.out.println("beforeAll");
+
+    }
+
+    @AfterAll
+    @DisplayName("表明在所有测试方法运行之后执行的方法")
+    static void afterAll(){
+        System.out.println("afterAll");
+
+    }
+
+    @BeforeEach
+    @DisplayName("每条用例开始时执行")
+    void start(){
+        System.out.println("start");
+    }
+
+    @AfterEach
+    @DisplayName("每条用例结束时执行")
+    void end(){
+        System.out.println("end");
+
+    }
+
+    @Test
+    @DisplayName("运行一组断言")
+    void assertAllCase() {
+        assertAll("groupAssert",() -> assertEquals(2, 1 + 1),() -> assertTrue(1 > 0));
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("disabled test")
+    public void ignoredTest() {
+        System.out.println("This test is disabled");
+    }
+}
