475b460b20e4b654816779d18f63c8270fa50e11
siegezhang <z1178858918@qq.com>
2019-05-05 22:43:53 +0800

    动态代理

--- src/main/java/invokedynamic/dynamicproxy/ProxyHandler.java
+++ src/main/java/invokedynamic/dynamicproxy/ProxyHandler.java
@@ -2,12 +2,19 @@ package invokedynamic.dynamicproxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class ProxyHandler implements InvocationHandler {
   private BeProxyInterface beProxyInterface;
 
   public ProxyHandler(BeProxyInterface beProxyInterface) {
     this.beProxyInterface = beProxyInterface;
+  }
+
+  public BeProxyInterface getProxy() {
+    return (BeProxyInterface)
+        Proxy.newProxyInstance(
+            this.getClass().getClassLoader(), new Class[] {BeProxyInterface.class}, this);
   }
 
   @Override
