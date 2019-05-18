a74f28b5e27e9fb573eafda85b1d7e85cc4898ca
siegezhang <z1178858918@qq.com>
2019-05-18 18:09:12 +0800

    动态代理cglib

--- src/main/java/cglib/Client.java
+++ src/main/java/cglib/Client.java
@@ -1,15 +1,31 @@
 package cglib;
 
+import net.sf.cglib.core.DebuggingClassWriter;
 import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
 import org.junit.jupiter.api.Test;
+
 /** https://www.jianshu.com/p/9a61af393e41?from=timeline&isappinstalled=0 */
 public class Client {
   @Test
   public void testCglib() {
+    System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "F:\\test");
     Enhancer enhancer = new Enhancer();
     enhancer.setSuperclass(HelloServiceImpl.class);
-    enhancer.setCallback(new HelloMethodInterceptor());
+    // enhancer.setCallback(new HelloMethodInterceptor());
+    enhancer.setCallback(
+        (MethodInterceptor)
+            (o, method, objects, methodProxy) -> {
+              System.out.println("Before:" + method.getName());
+              Object object = methodProxy.invokeSuper(o, objects);
+              System.out.println("After:" + method.getName());
+              return object;
+            });
+    // 我们也可以针对不同的方法调用不用的回调拦截，如下
+    // enhancer.setCallbacks(new Callback[] {new HelloMethodInterceptor()});
+    // enhancer.setCallbackFilter((Method method) -> 0);
     HelloServiceImpl helloService = (HelloServiceImpl) enhancer.create();
     helloService.sayHello();
+    helloService.toString();
   }
 }
--- src/main/java/cglib/HelloServiceImpl.java
+++ src/main/java/cglib/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package cglib;
 
+/** 委托类不能是final，同时被委托的方法也不能是final 如果是final类，则直接报错，如果是final方法，则不进行代理，直接执行该方法 */
 public class HelloServiceImpl {
   public void sayHello() {
     System.out.println("Hello,World");
