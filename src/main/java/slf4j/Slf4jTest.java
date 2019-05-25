6f188a4c600174697002006251c50d14647c9f8b
siegezhang <z1178858918@qq.com>
2019-05-25 10:33:10 +0800

    slf4j

---
+++ src/main/java/slf4j/Slf4jTest.java
@@ -0,0 +1,15 @@
+package slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+public class Slf4jTest {
+  @Test
+  public void testFactory() {
+    Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
+    logger.info("hello,{}", new Date());
+  }
+}
