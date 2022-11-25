b0d4d74a37c7e859764106e2b90784a2ab0bd372
zhangxiaosheng <z1178858918@qq.com>
2022-11-25 19:11:47 +0800

    commit

---
+++ src/main/java/jvmlearning/javaagent/AccountMain.java
@@ -0,0 +1,9 @@
+package jvmlearning.javaagent;
+
+public class AccountMain {
+  public static void main(String[] args) throws InterruptedException {
+    for (; ; ) {
+      Thread.sleep(5000);
+    }
+  }
+}
---
+++ src/main/java/springtest/HandlerMethodArgumentResolverTest.java
@@ -0,0 +1,11 @@
+package springtest;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class HandlerMethodArgumentResolverTest {
+  @Test
+  public void test() {
+    MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+  }
+}
