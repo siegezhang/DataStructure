4314c0474acdf83327d8aefd4db986547c31d555
zhangxiaosheng <z1178858918@qq.com>
2022-12-06 23:22:33 +0800

    commit

--- src/main/java/java8/function/IntPredicateTest.java
+++ src/main/java/java8/function/IntPredicateTest.java
@@ -2,7 +2,13 @@ package java8.function;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class IntPredicateTest {
-    @Test
-    public void test() {}
+  @Test
+  public void test() {
+    IntStream stream = IntStream.range(1, 10);
+    List<Integer> oddNumbers = stream.filter(argument -> argument % 2 == 1).boxed().toList();
+  }
 }
