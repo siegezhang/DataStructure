43aed352983f72c87fae1442bfc9f84d1d632ac5
zhangxiaosheng <z1178858918@qq.com>
2022-12-07 14:01:54 +0800

    commit

--- src/main/java/java8/function/IntPredicateTest.java
+++ src/main/java/java8/function/IntPredicateTest.java
@@ -1,12 +1,24 @@
 package java8.function;
 
+import io.reactivex.rxjava3.core.Observable;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
 public class IntPredicateTest {
+  public static <U> Function<U, IntPredicate> and(Function<U, IntPredicate>... filters) {
+    return user -> {
+      IntPredicate andPredicate = iidx -> true;
+      for (Function<U, IntPredicate> filter : filters) {
+        andPredicate = andPredicate.and(filter.apply(user));
+      }
+      return andPredicate;
+    };
+  }
+
   @Test
   public void test() {
     IntStream stream = IntStream.range(1, 10);
@@ -18,5 +30,16 @@ public class IntPredicateTest {
     IntPredicate i = (x) -> x < 0;
     IntPredicate j = (x) -> x == 0;
     System.out.println(i.and(j).test(123));
+  }
+
+  @Test
+  public void test2() {
+    final IntPredicate[] p = {(x) -> true};
+    IntStream primesStream =
+        IntStream.iterate(2, n -> n + 1)
+            .filter(i -> p[0].test(i))
+            .peek(i -> p[0] = p[0].and(v -> v % i != 0));
+    Observable<IntStream> primes = Observable.fromAction(() -> primesStream.iterator());
+    primes.take(10).forEach((x) -> System.out.println(x.toString()));
   }
 }
